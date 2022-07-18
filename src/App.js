cec194000939b3191242b3d18f2e5b859363e9a4
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-18 17:14:32 -0500

    Probando el primer PR

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function App() {
+    return (
+        <Typography variant="h1" color="initial">
+            Hola mundo!
+        </Typography>
+    );
+}
