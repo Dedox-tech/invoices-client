82df083c8f3500d81a7d6b8fc82a2cfa849ec536
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-23 01:16:26 -0500

    Reformatear con Prettier

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,19 @@
-import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import MenuIcon from '@mui/icons-material/Menu';
-import React from 'react'
+import {
+    AppBar,
+    Avatar,
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
 import { Link as LinkDom } from "react-router-dom";
 
 export default function NavBar() {
@@ -27,11 +39,11 @@ export default function NavBar() {
              </ul>
          </AppBar> */
         <AppBare />
-    )
+    );
 }
 
 // const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function AppBare() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -53,10 +65,12 @@ function AppBare() {
     };
 
     return (
-        <AppBar position="static" >
+        <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <AssignmentIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <AssignmentIcon
+                        sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+                    />
                     <Typography
                         variant="h6"
                         noWrap
@@ -64,18 +78,23 @@ function AppBare() {
                         href="/home"
                         sx={{
                             mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "monospace",
                             fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
                         }}
                     >
                         App Facturas
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -90,28 +109,38 @@ function AppBare() {
                             id="menu-appbar"
                             anchorEl={anchorElNav}
                             anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
+                                vertical: "bottom",
+                                horizontal: "left",
                             }}
                             keepMounted
                             transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
+                                vertical: "top",
+                                horizontal: "left",
                             }}
                             open={Boolean(anchorElNav)}
                             onClose={handleCloseNavMenu}
                             sx={{
-                                display: { xs: 'block', md: 'none' },
+                                display: { xs: "block", md: "none" },
                             }}
                         >
                             <MenuItem onClick={handleCloseNavMenu}>
                                 <Typography textAlign="center">
-                                    <LinkDom to="/invoices" style={{ color: 'black', textDecoration: 'none' }}>Invoices</LinkDom>
+                                    <LinkDom
+                                        to="/invoices"
+                                        style={{
+                                            color: "black",
+                                            textDecoration: "none",
+                                        }}
+                                    >
+                                        Invoices
+                                    </LinkDom>
                                 </Typography>
                             </MenuItem>
                         </Menu>
                     </Box>
-                    <AssignmentIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <AssignmentIcon
+                        sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+                    />
                     <Typography
                         variant="h5"
                         noWrap
@@ -119,48 +148,74 @@ function AppBare() {
                         href="/home"
                         sx={{
                             mr: 2,
-                            display: { xs: 'flex', md: 'none' },
+                            display: { xs: "flex", md: "none" },
                             flexGrow: 1,
-                            fontFamily: 'monospace',
+                            fontFamily: "monospace",
                             fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
                         }}
                     >
                         App Facturas
                     </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        <Button sx={{ my: 2, color: 'white', display: 'block' }}>
-                            <LinkDom to="/invoices" style={{ color: 'white', textDecoration: 'none' }}>Invoices</LinkDom>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        <Button
+                            sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                            <LinkDom
+                                to="/invoices"
+                                style={{
+                                    color: "white",
+                                    textDecoration: "none",
+                                }}
+                            >
+                                Invoices
+                            </LinkDom>
                         </Button>
                     </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{ p: 0 }}
+                            >
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="/static/images/avatar/2.jpg"
+                                />
                             </IconButton>
                         </Tooltip>
                         <Menu
-                            sx={{ mt: '45px' }}
+                            sx={{ mt: "45px" }}
                             id="menu-appbar"
                             anchorEl={anchorElUser}
                             anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
+                                vertical: "top",
+                                horizontal: "right",
                             }}
                             keepMounted
                             transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
+                                vertical: "top",
+                                horizontal: "right",
                             }}
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}
                         >
                             {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
+                                <MenuItem
+                                    key={setting}
+                                    onClick={handleCloseUserMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {setting}
+                                    </Typography>
                                 </MenuItem>
                             ))}
                         </Menu>
@@ -168,5 +223,5 @@ function AppBare() {
                 </Toolbar>
             </Container>
         </AppBar>
-    )
+    );
 }
