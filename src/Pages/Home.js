788cf83cd0a92469ea5110a3b01f2f5e1086c718
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-23 00:44:13 -0500

    Añadir link hacia /auth

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Button, Container, Grid } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import phone from "../images/cellPhone.png";
 import pie from "../images/pie-chart.png";
@@ -9,6 +10,12 @@ import NavBar from "../components/NavBar";
 import "../styles/Home.css";
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const handleClick = async () => {
+        navigate("/auth");
+    };
+
     return (
         <Box>
             <NavBar />
@@ -24,14 +31,20 @@ export default function Home() {
                 <Grid container sx={{ mt: 10 }}>
                     <Grid item xs={5}>
                         <Container sx={{ ml: 5, mb: 5 }}>
-                            <h2>La app que se encarga de tus facturas</h2>
-                            <p>
+                            <Typography variant="h4" mb={3}>
+                                La app que se encarga de tus facturas
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                color="initial"
+                                paragraph
+                            >
                                 Administrar el dinero puede ser complicado. No
                                 lo hagas solo. Nuestra app te ayudará a ahorrar
                                 más, gastar menos, ver todos tus movimientos y
                                 tomar el control de tu vida financiera.
-                            </p>
-                            <Button variant="contained">
+                            </Typography>
+                            <Button variant="contained" onClick={handleClick}>
                                 Sign in <ArrowRightAltIcon sx={{ ml: 1 }} />
                             </Button>
                         </Container>
