6fed9f67955a0f28d7873b05f02afb4fb86ef04f
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-25 22:38:07 -0500

    Añadir boton flotante para editar las facturas

--- src/components/FloatingButtonEdit.js
+++ src/components/FloatingButtonEdit.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Fab } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
 
-export default function FloatingButtonAdd() {
+export default function FloatingButtonEdit() {
     return (
         <Fab
             color="primary"
-            aria-label="add"
+            aria-label="edit"
             sx={{
                 position: "fixed",
                 bottom: (theme) => theme.spacing(3),
                 right: (theme) => theme.spacing(3),
             }}
         >
-            <AddIcon />
+            <EditIcon />
         </Fab>
     );
 }
--- src/pages/EditInvoice.js
+++ src/pages/EditInvoice.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import InvoiceEditText from "../components/InvoiceEditText";
 import InvoiceEditTable from "../components/InvoiceEditTable";
+import FloatingButtonEdit from "../components/FloatingButtonEdit";
 import NavBar from "../components/NavBar";
 
 export default function EditInvoice() {
@@ -10,6 +11,7 @@ export default function EditInvoice() {
             <NavBar />
             <InvoiceEditText />
             <InvoiceEditTable />
+            <FloatingButtonEdit />
         </Box>
     );
 }
