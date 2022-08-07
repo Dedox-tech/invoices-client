b26a71257184d84f344c97c25e5ec727e0e8c6f6
Diego Murillo <diegomurilloferrer@gmail.com>
2022-08-07 09:38:35 -0500

    Update env variables

--- src/utils/data-fetching/useGetUserInfo.js
+++ src/utils/data-fetching/useGetUserInfo.js
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 export default function useGetUserInfo() {
     return useQuery("getUserInfo", async () => {
         const response = await fetch(
-            `${process.env.REACT_APP_API_DOMAIN}/users/current`
+            `${process.env.NODE_ENV.REACT_APP_API_DOMAIN}/users/current`
         );
         if (!response.ok) {
             throw new Error(
